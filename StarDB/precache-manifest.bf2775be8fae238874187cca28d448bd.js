f081cd20c83843a3dca28f866b242ef5bc43ef4c
TazmanS <TazmanS123@gmail.com>
2019-04-20 12:23:34 +0300

    Test

---
+++ StarDB/precache-manifest.bf2775be8fae238874187cca28d448bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bad597b89defe2f2d7b0",
+    "url": "/static/css/main.76f75f19.chunk.css"
+  },
+  {
+    "revision": "bad597b89defe2f2d7b0",
+    "url": "/static/js/main.bad597b8.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ab76faf0308d38fa7a58",
+    "url": "/static/js/2.ab76faf0.chunk.js"
+  },
+  {
+    "revision": "b508dbd18d441668e012f1506496c6ec",
+    "url": "/index.html"
+  }
+];
