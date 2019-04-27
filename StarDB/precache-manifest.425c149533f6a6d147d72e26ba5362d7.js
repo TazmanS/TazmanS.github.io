e2f09f404aa78f3289678db5855adbea82fbcca2
TazmanS <TazmanS123@gmail.com>
2019-04-27 18:45:36 +0300

    Res

---
+++ StarDB/precache-manifest.425c149533f6a6d147d72e26ba5362d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "abc37fa28326c08924fa",
+    "url": "/static/css/main.50f5b580.chunk.css"
+  },
+  {
+    "revision": "abc37fa28326c08924fa",
+    "url": "/static/js/main.abc37fa2.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ab76faf0308d38fa7a58",
+    "url": "/static/js/2.ab76faf0.chunk.js"
+  },
+  {
+    "revision": "e27fa24de1dc89422c95734e30827a62",
+    "url": "/index.html"
+  }
+];
--- StarDB/service-worker.js
+++ StarDB/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.72f5ccefc4117856c20186dd1d1a1d85.js"
+  "/precache-manifest.425c149533f6a6d147d72e26ba5362d7.js"
 );
 
 workbox.clientsClaim();
