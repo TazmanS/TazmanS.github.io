c5faccc6b070c4c8efc3156dadc9476f7d27038d
TazmanS <tazmans123@gmail.com>
2019-07-16 21:25:38 +0300

    first commit

---
+++ messenger/precache-manifest.c71a46e0fc20d6c448f6f89b76970f9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e65d415538578b8e82fa0a41fec3e247",
+    "url": "/index.html"
+  },
+  {
+    "revision": "beb89220874cd3a5bf38",
+    "url": "/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "7bfbc5e8904cbc476978",
+    "url": "/static/css/main.871f2847.chunk.css"
+  },
+  {
+    "revision": "beb89220874cd3a5bf38",
+    "url": "/static/js/2.7e2a6928.chunk.js"
+  },
+  {
+    "revision": "7bfbc5e8904cbc476978",
+    "url": "/static/js/main.8409ef06.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
