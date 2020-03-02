17fa6ac34f418f9261f7ecbf6207a25fc3c851cb
TazmanS <tazmans123@gmail.com>
2020-03-02 19:44:24 +0200

    todolist

---
+++ messenger/precache-manifest.707a226c7a2d12d740b85639145c5826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa6c3147403fdadd37dfbe34b7e1e0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1202415dadd276fe06ec",
+    "url": "/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "69ca1661437f8777cee5",
+    "url": "/static/css/main.6ad7658a.chunk.css"
+  },
+  {
+    "revision": "1202415dadd276fe06ec",
+    "url": "/static/js/2.ac00506f.chunk.js"
+  },
+  {
+    "revision": "69ca1661437f8777cee5",
+    "url": "/static/js/main.b959c90c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
